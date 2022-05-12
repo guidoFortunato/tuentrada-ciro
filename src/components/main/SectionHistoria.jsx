627f9542a390130aedb1c0a7b473ac6a21c1fe0f
guido fortunato <guido_fortunato@hotmail.com.ar>
2022-05-12 11:56:53 -0300

    cambios

--- src/components/main/SectionHistoria.jsx
+++ src/components/main/SectionHistoria.jsx
@@ -1,40 +1,29 @@
-import React from 'react'
-import { VariablesContext } from '../../context/VariablesProvider'
+import React from "react";
+import { VariablesContext } from "../../context/VariablesProvider";
 
 const SectionHistoria = () => {
+  const { variables } = React.useContext(VariablesContext);
 
-    const {variables} = React.useContext(VariablesContext)
+  return (
+    <>
+      <section id="about">
+        <div className="container about-container wow fadeInUp">
+          <div className="row">
+            <div className="col-12 col-lg-6 about-img mb-2 mb-lg-0">
+              <img src={variables.imagenHistoria} alt={variables.venueName} />
+            </div>
 
-    return (
-        <>
-          
-                <section id="about" className='mt-3'>
-                    
+            <div className="col-12 col-lg-6 about-content">
+              <p className="about-text">{variables.descripcion1}</p>
 
-                    <div className="container about-container wow fadeInUp">
-                        <div className="row">
+              <p className="about-text">{variables.descripcion2}</p>
+            </div>
+          </div>
+        </div>
+        {/* <div className="section-title-divider mt-4"></div> */}
+      </section>
+    </>
+  );
+};
 
-                            <div className="col-12 col-lg-6 about-img mb-2 mb-lg-0">
-                                <img src={variables.imagenHistoria} alt={variables.venueName} />
-                            </div>
-
-                            <div className="col-12 col-lg-6 about-content">
-                                <p className="about-text">
-                                    {variables.descripcion1}
-                                </p>
-                            
-                                <p className="about-text">
-                                    {variables.descripcion2}
-                                </p>
-                                
-                            </div>
-                        </div>
-                    </div>
-                    {/* <div className="section-title-divider mt-4"></div> */}
-                </section>
-            
-        </>
-    )
-}
-
-export default SectionHistoria
+export default SectionHistoria;
